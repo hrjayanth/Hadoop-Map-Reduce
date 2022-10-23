47add6905342adb790fec43b850d5e123e16c02d
hrjayanth <hrjayanth@gmail.com>
2022-10-23 21:24:56 +0530

    Adding the solution for ProblemStatement1

---
+++ ProblemStatement1/reducer2.py
@@ -0,0 +1,17 @@
+import sys
+
+scores=[]
+for line in sys.stdin:
+    line = line.strip()
+    batsman,batsman_runs = line.split('\t')
+    try:
+        batsman_runs = int(batsman_runs)
+        batsman = batsman.strip()
+    except ValueError:
+        continue
+    scores.append([batsman_runs, batsman])
+
+top_N=sorted(scores,reverse=True)[0:10]
+
+for t in top_N:
+    print(t[1], '\t', t[0] )
